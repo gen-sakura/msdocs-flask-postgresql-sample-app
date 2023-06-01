84f641385f834dc5fa6716f228dec0f4c3df5a55
gen-sakura <gensakura02@gmail.com>
2023-06-01 02:24:02 -0700

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -3,8 +3,8 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['oystercloud2.azurewebsites.net']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['oystercloud2.azurewebsites.net']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
@@ -12,8 +12,8 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
+    dbuser=conn_str_params['mrttvqovox'],
+    dbpass=conn_str_params['Y270OQ2LR85MAP84'],
+    dbhost=conn_str_params['oystercloud2-server.postgres.database.azure.com'],
+    dbname=conn_str_params['oystercloud2-database']
 )
